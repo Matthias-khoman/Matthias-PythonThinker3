c43ec583f49c750f069face2bd21243c44d64afe
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 04:29:32 +0000

    Jan 19, 2025, 4:29 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -46,7 +46,7 @@ menu = {"Cheeseburger": 5.50,
             "Soda": 2.00}
 
 def display_menu():
-    print("{:Welcome to HanBaoBao")
+    print("{:^10}Welcome to HanBaoBao")
     print("Here is the menu\n")
     for food, price in menu.items():
         print(f"{food} costs {price}")
