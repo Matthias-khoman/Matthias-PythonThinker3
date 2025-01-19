1cfaef3c1dc332b23d766901fb9bed7d55e74c65
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 03:06:33 +0000

    Jan 19, 2025, 3:06 AM

--- .vscode/CT08_01/lesson1.py
+++ .vscode/CT08_01/lesson1.py
@@ -32,6 +32,6 @@ def bubble_sort(numbers):
             temp = numbers[n + 1]
             numbers[n + 1] = numbers[n]
             numbers[n] = temp
-            return numbers
+        return numbers
 
 print(bubble_sort(list1))
