cd265ac1804d5af388767d9b270109a1d9231eac
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 03:56:17 +0000

    Jan 19, 2025, 3:56 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -20,5 +20,5 @@ del student_grade['John']
 print(f"Students {student_grade}")
 
 # Task 6
-if student_grade['Michael'] in student_grade:
+if 'Michael' in student_grade:
     print("Michael is in the dictionary")
