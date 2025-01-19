9c2d510cea1ed694f430a04d51571b9746892172
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 04:02:45 +0000

    Jan 19, 2025, 4:02 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -26,6 +26,7 @@ student_grade = {"Matthias": 98, "John": 89, "Michael": 85, "Mabel":60}
 # Task 7
 for student in student_grade:
     grade = student_grade[student]
+    print
     
     print(student)
     break
