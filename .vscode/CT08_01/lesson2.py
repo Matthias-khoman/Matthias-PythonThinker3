5e65a5f5c0374dda971ea5bc334c056bd9e6e2ee
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 03:44:55 +0000

    Jan 19, 2025, 3:44 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -3,4 +3,4 @@ student_grade = {"Matthias": 98, "John": 89, "Michael": 85}
 print(f"Student grades {student_grade}")
 
 # Task 2
-print(student_grade["Matthias"])
+print(f" The Grade of Matthias is {student_grade["Matthias"]}")
