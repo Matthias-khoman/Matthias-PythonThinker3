c4778ad911c721b0ae83888afaca8d800e63bdcb
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 03:59:57 +0000

    Jan 19, 2025, 3:59 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -1,23 +1,23 @@
 # Task 1
-student_grade = {"Matthias": 98, "John": 89, "Michael": 85}
-print(f"Student grades {student_grade}")
+# student_grade = {"Matthias": 98, "John": 89, "Michael": 85}
+# print(f"Student grades {student_grade}")
 
-# Task 2
-print(f" The Grade of Matthias is {student_grade["Matthias"]}")
+# # Task 2
+# print(f" The Grade of Matthias is {student_grade["Matthias"]}")
 
-# Task 3
-name = "Marc"
-grade = 100
-student_grade[name] = grade
-print(f" Students: {student_grade}")
+# # Task 3
+# name = "Marc"
+# grade = 100
+# student_grade[name] = grade
+# print(f" Students: {student_grade}")
 
-# Task 4
-student_grade["John"] = 10
-print(f" Students: {student_grade}")
+# # Task 4
+# student_grade["John"] = 10
+# print(f" Students: {student_grade}")
 
-# Task 5
-del student_grade['John']
-print(f"Students {student_grade}")
+# # Task 5
+# del student_grade['John']
+# print(f"Students {student_grade}")
 
 # Task 6
 # if 'Michael' in student_grade:
@@ -26,3 +26,4 @@ print(f"Students {student_grade}")
 # Task 7
 for student in student_grade:
     print(student)
+    break
