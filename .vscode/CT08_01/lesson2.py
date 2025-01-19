14151e0971bc982e4b501d367f0ed6e8cc25d606
Matthias-khoman <matthiaskhoman@gmail.com>
2025-01-19 04:22:23 +0000

    Jan 19, 2025, 4:22 AM

--- .vscode/CT08_01/lesson2.py
+++ .vscode/CT08_01/lesson2.py
@@ -48,7 +48,7 @@ def display_menu():
             "Cheese Fries": 4.50,
             "Soda": 2.00}
     for food, price in menu:
-        print(f"{food} ")
+        print(f"{food} costs {price}")
     print("end of menu")
 
 display_menu()
