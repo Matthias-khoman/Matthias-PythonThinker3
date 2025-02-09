9b46d498396064bc3e6cecdca0540d03670338d2
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-09 03:50:55 +0000

    Feb 9, 2025, 3:50 AM

--- .vscode/CT08_01/lessom3.py
+++ .vscode/CT08_01/lessom3.py
@@ -31,7 +31,7 @@ def order_summary(customer_order):
 
 display_BOS()
 order = input('What do you want? (Say no more to end)')
-
+quantity = input()
 while order != 'no more':
     take_order(BOS, order, customer_order)
     order = input('What do you want? (Say no more to end)')
