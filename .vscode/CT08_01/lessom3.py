423050d47745f1cc0efb23bf26bbc5d3759e1f66
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-09 03:18:58 +0000

    Feb 9, 2025, 3:18 AM

--- .vscode/CT08_01/lessom3.py
+++ .vscode/CT08_01/lessom3.py
@@ -11,8 +11,6 @@ customer_order={}
 
 def take_order(menu, order, customer_order):
     if order in BOS:
-        option = input(f'{order} costs {BOS[order]} would you like it? ')
-        if option == 'y':
-            print(f'{order}')
+        
 
         
