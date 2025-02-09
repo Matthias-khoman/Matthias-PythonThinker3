c2e691bfb8dad9881018d80cfb9b52339dbab07f
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-09 03:43:21 +0000

    Feb 9, 2025, 3:43 AM

--- .vscode/CT08_01/lessom3.py
+++ .vscode/CT08_01/lessom3.py
@@ -24,8 +24,8 @@ def order_summary(customer_order):
         quantity = quantity_cost[quantity]
         cost = quantity_cost[cost]
         print(f'{order} x {quantity} at {cost} each')
-        total_price = 0
-        Tota
+        total_item_price = 0
+        
 
 
         
