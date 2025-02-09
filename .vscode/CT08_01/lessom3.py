d3dc3b2994fb8254b5b5b1d01216b124e3b1b250
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-09 03:51:35 +0000

    Feb 9, 2025, 3:51 AM

--- .vscode/CT08_01/lessom3.py
+++ .vscode/CT08_01/lessom3.py
@@ -35,7 +35,7 @@ quantity = input('hpw many do you want')
 while order != 'no more':
     take_order(BOS, order, customer_order)
     order = input('What do you want? (Say no more to end)')
-    quanti
+    quantity = input('How many do you want? ')
 order_summary()
 
 
