90157385ff83a0ad6b69cad6b785fc8f6801d55e
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-09 03:31:14 +0000

    Feb 9, 2025, 3:31 AM

--- .vscode/CT08_01/lessom3.py
+++ .vscode/CT08_01/lessom3.py
@@ -16,8 +16,7 @@ def take_order(BOS, order, quantity,  customer_order):
     else:
         print("We don't sell that")
 
-take_order(BOS, 'Notebook', 2, customer_order)
-print(customer_order)
+
 
 # def order_summary(customer_order):
 #     print('-----------Order Summary------------')
