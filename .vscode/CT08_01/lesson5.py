f0437b3e63fc22754da474e13db3f5bce9942975
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-16 03:27:45 +0000

    Feb 16, 2025, 3:27 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -3,15 +3,15 @@ students = {"peter": [True, True, True], "lily": [False, False, True], "alice": 
 
 def take_attendance(students: dict) -> dict:
     for student, attendance in students.items():
-        who
-        attendance = input(f'Is {student} present(y/n)? ')
-        if attendance == 'y':
-            attendance = True
-        elif attendance == 'n':
-            attendance = False
-        else:
-            print('invalid input only y or n')
-        students[student].append(attendance)
+        while True:
+            attendance = input(f'Is {student} present(y/n)? ')
+            if attendance == 'y':
+                attendance = True
+            elif attendance == 'n':
+                attendance = False
+            else:
+                print('invalid input only y or n')
+            students[student].append(attendance)
     print('Attendance for Class A is taken')
     return students
 
