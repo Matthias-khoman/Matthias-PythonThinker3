69fb2b47a5d91a944b83d14834bd1230ac0e9b45
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-16 04:12:13 +0000

    Feb 16, 2025, 4:12 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -38,4 +38,4 @@ print(value)
 def notify_low_attendance_students(students: dict, threshold: float) -> list:
     low_attendance_students = []
     for student, attendance in students:
-       attendance_percentattennce_percent(student, students)
+       attendance_percent=
