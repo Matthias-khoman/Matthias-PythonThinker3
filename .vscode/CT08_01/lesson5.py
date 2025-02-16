953f3cedc8e784aed5efdedbc1288a7b4b69b793
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-16 03:12:21 +0000

    Feb 16, 2025, 3:12 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -1,2 +1,2 @@
 # Task 1
-stsudents = {"peter": [True, True, True], "lily":[False, False, True], "alice": [False, False, False], "tom": [True, True]}
+stsudents = {"peter": [True, True, True], "lily":[False, False, True], "alice": [False, False, False], "tom": [True, True, False]}
