13cab1933d69efdac8ee160bb9aaa7d743c0ef8d
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-16 03:43:18 +0000

    Feb 16, 2025, 3:43 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -31,7 +31,7 @@ def attendance_percent(student: str, students: dict) -> float:
                     num_true +=1
                     break
             total = len(attendance)
-            
+
 
             
         
