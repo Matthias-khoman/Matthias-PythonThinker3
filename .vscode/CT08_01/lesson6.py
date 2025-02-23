2ccb3cd5528603415b83548c0758b50108c6a25f
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-23 03:58:59 +0000

    Feb 23, 2025, 3:58 AM

--- .vscode/CT08_01/lesson6.py
+++ .vscode/CT08_01/lesson6.py
@@ -3,3 +3,10 @@ student_answers = {'john': ['A', 'C', 'B', 'D'],
                    'jane': ['A', 'B', 'B', 'D'],
                    'alice': ['A', 'C', 'C', 'D'],
                    'bob': ['A', 'B', 'B', 'D']}
+
+def grade_students(student_answers: dict, answer_key: list):
+    student_score = {}
+    for student, answers in student_answers.items():
+        score = 0
+        for i in range(len(answer_key)):
+            i
