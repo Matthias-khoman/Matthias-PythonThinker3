84926dd4a683e77c4352e38391b2bbd27ef54a63
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-23 03:08:21 +0000

    Feb 23, 2025, 3:08 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -52,7 +52,7 @@ choice = input('Enter your choice: ')
 if choice == 1:
     take_attendance(students)
 elif choice == 2:
-    student_name = input
+    student_name = input('Enter the name: ')
     attendance_percent()
 elif choice == 3:
     notify_low_attendance_students()
