78b0437fd7d3fc58c9462bee39dc9a2f7c21c35d
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-23 03:29:35 +0000

    Feb 23, 2025, 3:29 AM

--- .vscode/CT08_01/lesson5.py
+++ .vscode/CT08_01/lesson5.py
@@ -61,7 +61,7 @@ while True:
         student_name = input('Enter the name:(lowercase) ').lower()
         attendance_percent(student_name, students)
     elif choice == 3:
-        threshold = 
+        threshold = float(input(''))
         list_warning = notify_low_attendance_students(students)
         print(f'The list of students with low attendance is {list_warning}')
     elif choice == 4:
