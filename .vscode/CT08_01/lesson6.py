53c86eb9445f486fec929d214c77e6e84a794c33
Matthias-khoman <matthiaskhoman@gmail.com>
2025-02-23 03:40:05 +0000

    Feb 23, 2025, 3:40 AM

--- .vscode/CT08_01/lesson6.py
+++ .vscode/CT08_01/lesson6.py
@@ -2,4 +2,4 @@ answer_key = ['A', 'B', 'C', 'D']
 student_answers = {'john': ['A', 'C', 'B', 'D'],
                    'jane': ['A', 'B', 'B', 'D'],
                    'alice': ['A', 'C', 'C', 'D'],
-                   'bob': ['A', 'B', '']}
+                   'bob': ['A', 'B', 'B', 'D']}
