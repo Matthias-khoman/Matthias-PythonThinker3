f61eaebbedddf43a189fbba0333689b8dc4bf901
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-09 03:47:30 +0000

    Mar 9, 2025, 3:47 AM

--- .vscode/CT08_01/lesson7.py
+++ .vscode/CT08_01/lesson7.py
@@ -59,7 +59,7 @@ while num <= 5:
     # word input
     word = input('What word do you want? ')
     # get the number of letter in the word
-    num_of_letters = len(word)
+    num_of_letters = list(word)
     # print(num_of_letters)
     # To find the number of points for each letter
     for letter in range(num_of_letters):
