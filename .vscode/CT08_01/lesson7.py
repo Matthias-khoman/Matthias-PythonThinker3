a0e9fcee9c94c0265deaca33a1bb5584acd52872
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-09 03:17:52 +0000

    Mar 9, 2025, 3:17 AM

--- .vscode/CT08_01/lesson7.py
+++ .vscode/CT08_01/lesson7.py
@@ -1,42 +1,42 @@
-import os 
-# Exercise 0
-filepath = os.getcwd()
+# import os 
+# # Exercise 0
+# filepath = os.getcwd()
 
-fullpath = os.path.join(filepath, "example.txt")
+# fullpath = os.path.join(filepath, "example.txt")
 
-if os.path.exists(fullpath):
-    print("{} exist".format(fullpath))
-else:
-    print("{} does not exist".format(fullpath))
+# if os.path.exists(fullpath):
+#     print("{} exist".format(fullpath))
+# else:
+#     print("{} does not exist".format(fullpath))
 
-# Exercise 1
-file = open(fullpath, "w")
-file.write("Hello John")
-file.close()
+# # Exercise 1
+# file = open(fullpath, "w")
+# file.write("Hello John")
+# file.close()
 
-# Exercise 2
-file = open(fullpath, "r")
-content = file.read()
-print(f'File Content {content}')
-file.close()
+# # Exercise 2
+# file = open(fullpath, "r")
+# content = file.read()
+# print(f'File Content {content}')
+# file.close()
 
-# Exercise 3
-with open(fullpath, 'r') as file:
-    content = file.read()
-    print(f"File content with 'with': {content}")
+# # Exercise 3
+# with open(fullpath, 'r') as file:
+#     content = file.read()
+#     print(f"File content with 'with': {content}")
 
-# Exercise 4
-with open(fullpath, "a") as file:
-    file.write("\nThis is John")
-    file.write("\nThis is John Doe")
+# # Exercise 4
+# with open(fullpath, "a") as file:
+#     file.write("\nThis is John")
+#     file.write("\nThis is John Doe")
 
-# Exercise 5
-lines = ['Line 1\n', 'Line 2\n', 'Line 3\n']
-with open(fullpath, "w") as file:
-    file.writelines(lines)
+# # Exercise 5
+# lines = ['Line 1\n', 'Line 2\n', 'Line 3\n']
+# with open(fullpath, "w") as file:
+#     file.writelines(lines)
 
-with open(fullpath, "r") as file:
-    lines = file.readline()
+# with open(fullpath, "r") as file:
+#     lines = file.readline()
 
 
 my_list = [5, 8, 10, 9, 11, 12, 15, 2]
