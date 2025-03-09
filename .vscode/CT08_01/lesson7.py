d5b37eb45d386dc355071093b32f8ceaef8e969b
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-09 03:44:47 +0000

    Mar 9, 2025, 3:44 AM

--- .vscode/CT08_01/lesson7.py
+++ .vscode/CT08_01/lesson7.py
@@ -58,7 +58,7 @@ num = 0
 while num <= 5:
     # word input
     word = input('What word do you want? ')
-    
+    num_of_letters = len(word)
     # To find the number of points for each letter
     if letter in range(len(word)):
         # Declare score variable
