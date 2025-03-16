bf932d70e2308d3331704e2711f124dad5077819
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-16 04:32:03 +0000

    Hi

--- .vscode/CT08_01/lesson8.py
+++ .vscode/CT08_01/lesson8.py
@@ -3,7 +3,7 @@ import os
 filename = 'tasks.txt'
 filepath = os.getcwd()
 fullpath = os.path.join(filepath, filename)
-def create_file():
+def create_file(fullpath: str):
     if not os.path.exists(fullpath):
         with open(fullpath, 'w') as file:
             file.write('My Task list:')
@@ -17,4 +17,36 @@ def create_file():
             print('\nOK, creating a new file')
 
 
-create_file()
+# create_file()
+# Task 2: Add new task
+def add_new_task(fullpath: str):
+    task = input('What is your task? ')
+    with open(fullpath, 'a') as file:
+        file.write(f'\n{task}')
+    print('Task added')
+
+# add_new_task()
+
+# Task 3: View all tasks
+def view_all_tasks(fullpath: str):
+    with open(fullpath, 'r') as file:
+        tasks = file.readlines()
+    if len(tasks) == 1:
+        print('NO TASKS FOUND')
+        return []
+    for i in range(len(tasks)):
+        if i == 0:
+            print(tasks[i].strip())
+        else:
+            print(f'{i}. {tasks[i]}'.strip())
+    
+
+# view_all_tasks(fullpath)
+
+# Task 4: Mark Task as done
+def mark_task_as_done(fullpath):
+    td = int(input('Which task have you done? '))
+    tasks = view_all_tasks(fullpath)
+    for i in range(len(tasks)):
+        if i == td:
+            with open(fullpath, 'a') as file:
