d4e5ec6d7c0ede35d14412afa19e18d55d046de1
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-16 03:21:02 +0000

    Mar 16, 2025, 3:21 AM

--- .vscode/CT08_01/lesson8.py
+++ .vscode/CT08_01/lesson8.py
@@ -1,5 +1,8 @@
 import os
 # Task 1: Create new task file
+filename = 'tasks.txt'
+filepath = os.getcwd()
+fullpath = os.path.join(filepath, filename)
 def create_file():
-    filepath = os.getcwd()
+    
     
