43ecd4a6218824f3ad7174ca3e489b1a9e67c565
Matthias-khoman <matthiaskhoman@gmail.com>
2025-03-16 03:24:15 +0000

    Mar 16, 2025, 3:24 AM

--- .vscode/CT08_01/lesson8.py
+++ .vscode/CT08_01/lesson8.py
@@ -7,7 +7,7 @@ def create_file():
     global fullpath
     if not os.path.exists(fullpath):
         os.makedirs(fullpath)
-        print('\nOK, ')
+        print('\nOK, creating a new file')
     else:
         print('file exists')
 
